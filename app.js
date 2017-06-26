5aa2d043d0b8e46ee22601e6a5e6a05b233208ea
Dotos <yy.liu@foxmail.com>
2017-06-26 16:39:22 +0800

    update mkts

--- app.js
+++ app.js
@@ -32,12 +32,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-var BING_MARKETS = ["zh-cn", "ar-xa", "bg-bg", "cs-cz", "da-dk", "de-at", "de-ch", "de-de", "el-gr", "en-a", "en-ca", "en-gb", "en-id", "en-ie", "en-in", "en-my", "en-nz", "en-ph", "en-sg", "en-us", "en-xa", "en-za", "es-ar", "es-cl", "es-es", "es-mx", "es-us", "es-xl", "et-ee", "fi-fi", "fr-be", "fr-ca", "fr-ch", "fr-fr", "he-il", "hr-hr", "hu-h", "it-it", "ja-jp", "ko-kr", "lt-lt", "lv-lv", "nb-no", "nl-be", "nl-nl", "pl-pl", "pt-br", "pt-pt", "ro-ro", "ru-r", "sk-sk", "sl-sl", "sv-se", "th-th", "tr-tr", "uk-ua", "zh-hk", "zh-tw"];
+//var BING_MARKETS = ["zh-cn", "ar-xa", "bg-bg", "cs-cz", "da-dk", "de-at", "de-ch", "de-de", "el-gr", "en-a", "en-ca", "en-gb", "en-id", "en-ie", "en-in", "en-my", "en-nz", "en-ph", "en-sg", "en-us", "en-xa", "en-za", "es-ar", "es-cl", "es-es", "es-mx", "es-us", "es-xl", "et-ee", "fi-fi", "fr-be", "fr-ca", "fr-ch", "fr-fr", "he-il", "hr-hr", "hu-h", "it-it", "ja-jp", "ko-kr", "lt-lt", "lv-lv", "nb-no", "nl-be", "nl-nl", "pl-pl", "pt-br", "pt-pt", "ro-ro", "ru-r", "sk-sk", "sl-sl", "sv-se", "th-th", "tr-tr", "uk-ua", "zh-hk", "zh-tw"];
+var BING_MARKETS = ["de-de", "en-au", "en-ca", "en-gb", "en-in", "en-us", "fr-ca", "fr-fr", "ja-jp", "pt-br", "zh-cn"];
 let k = 0;
 /**
- * 每隔2分钟检测bing数据
+ * 每隔3分钟检测bing数据
  */
-schedule.scheduleJob('*/2 * * * *', () => {
+schedule.scheduleJob('*/3 * * * *', () => {
     let mkt = BING_MARKETS[k];
     k = k < BING_MARKETS.length - 1 ? ++k : 0
     let config = {
